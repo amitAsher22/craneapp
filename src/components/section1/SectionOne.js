c234fdb7433c89148980770c11d6550874ee18f5
amita <amitasher4@gmail.com>
2022-06-20 12:30:53 +0300

    ddas

--- src/components/section1/SectionOne.js
+++ src/components/section1/SectionOne.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../Css/SectionOne.css";
 
 function SectionOne() {
+  const [open, setOpen] = useState(false);
   return (
     <div>
-      <div className="mainDivOne">
-        <div className="titleNav">
-          <span>עמוד הבית</span>
+      <p className="close" onClick={() => setOpen(!open)}>
+        {open ? (
+          <span className="bgClose">❌</span>
+        ) : (
+          <span className="bgOpen">🍔</span>
+        )}
+      </p>
+      {open ? (
+        <div className="mainDivOne">
+          <div className="titleNav">
+            <span>עמוד הבית</span>
+          </div>
+          <div className="titleNav">
+            <span>מי אנחנו</span>
+          </div>
+          <div className="titleNav">
+            <span>השירותים שלנו</span>
+          </div>
+          <div className="titleNav">
+            <span>יצירת קשר</span>
+          </div>
         </div>
-        <div className="titleNav">
-          <span>מי אנחנו</span>
-        </div>
-        <div className="titleNav">
-          <span>השירותים שלנו</span>
-        </div>
-        <div className="titleNav">
-          <span>יצירת קשר</span>
-        </div>
-      </div>
+      ) : null}
     </div>
   );
 }
